5b8d62d46d07903350b97ed28f2fbec69c9bba15
Christian E. Hopps <chopps@gmail.com>
2015-05-01 06:58:41 -0400

    Update to 24.5 and sync with std formula.

--- emacs.rb
+++ emacs.rb
@@ -2,42 +2,35 @@ require "formula"
 
 class Emacs < Formula
   homepage "https://www.gnu.org/software/emacs/"
-  url "http://ftpmirror.gnu.org/emacs/emacs-24.4.tar.xz"
-  mirror "https://ftp.gnu.org/pub/gnu/emacs/emacs-24.4.tar.xz"
-  sha256 "47e391170db4ca0a3c724530c7050655f6d573a711956b4cd84693c194a9d4fd"
+  url "http://ftpmirror.gnu.org/emacs/emacs-24.5.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz"
+  sha256 "dd47d71dd2a526cf6b47cb49af793ec2e26af69a0951cc40e43ae290eacfc34e"
 
-  option "cocoa", "Build a Cocoa version of emacs"
-  option "with-x", "Include X11 support"
-  option "use-git-head", "Use Savannah (faster) git mirror for HEAD builds"
-  option "keep-ctags", "Don't remove the ctags executable that emacs provides"
+  conflicts_with 'emacs',
+      :because => 'both install emacs executables.'
 
-  head do
-    if build.include? "use-git-head"
-      url "http://git.sv.gnu.org/r/emacs.git"
-    else
-      url "bzr://http://bzr.savannah.gnu.org/r/emacs/trunk"
-    end
-
+  devel do
+    url "http://git.sv.gnu.org/r/emacs.git", :branch => "emacs-24"
+    version "24.5-dev"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
 
-  # Fix ns-antialias-text, broken in 24.4, submitted upstream here:
-  # http://lists.gnu.org/archive/html/emacs-devel/2014-10/msg00813.html
-
-  patch do
-    url 'https://gist.githubusercontent.com/scotchi/66edaf426d7375c0f061/raw/b7055ba40a7dd9e8f6f5dd6bbe5c305a78bbbc87/emacs-fix-ns-antialias-text-mac-os.patch'
-    sha1 '6215c59c01dc247dfdec7c89ff2fe84ff28eb1c7'
+  head do
+    url "http://git.sv.gnu.org/r/emacs.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
   end
 
-  # Apply patch to support 24-bit colors in a terminal.
-  patch :p0 do
-    url "https://gist.githubusercontent.com/choppsv1/36aacdd696d505566088/raw/9f9f888ea987cfb9a2ea32a15947e16cb0672279/emacs-24.4-24bit.diff"
-    sha1 "4b03cf852314634ca5c49ce4a5698c57bfa46b30"
-  end
+  option "with-cocoa", "Build a Cocoa version of emacs"
+  option "with-ctags", "Don't remove the ctags executable that emacs provides"
+
+  deprecated_option "cocoa" => "with-cocoa"
+  deprecated_option "keep-ctags" => "with-ctags"
+  deprecated_option "with-x" => "with-x11"
 
   depends_on "pkg-config" => :build
-  depends_on :x11 if build.with? "x"
+  depends_on :x11 => :optional
   depends_on "d-bus" => :optional
   depends_on "gnutls" => :optional
   depends_on "librsvg" => :optional
@@ -45,38 +38,43 @@ class Emacs < Formula
   depends_on "mailutils" => :optional
   depends_on "glib" => :optional
 
+  # https://github.com/Homebrew/homebrew/issues/37803
+  if build.with? "x11"
+    depends_on "freetype" => :recommended
+    depends_on "fontconfig" => :recommended
+  end
+
   fails_with :llvm do
     build 2334
     cause "Duplicate symbol errors while linking."
   end
 
   def install
-    # HEAD builds blow up when built in parallel as of April 20 2012
-    # FIXME is this still necessary? It's been more than two years, surely any
-    # race conditions would have made it into release by now.
-    ENV.deparallelize unless build.stable?
-
     args = ["--prefix=#{prefix}",
             "--enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp",
             "--infodir=#{info}/emacs"]
+
     args << "--with-file-notification=gfile" if build.with? "glib"
+
     if build.with? "d-bus"
       args << "--with-dbus"
     else
       args << "--without-dbus"
     end
+
     if build.with? "gnutls"
       args << "--with-gnutls"
     else
       args << "--without-gnutls"
     end
+
     args << "--with-rsvg" if build.with? "librsvg"
     args << "--with-imagemagick" if build.with? "imagemagick"
     args << "--without-popmail" if build.with? "mailutils"
 
-    system "./autogen.sh" unless build.stable?
+    system "./autogen.sh" if build.head? || build.devel?
 
-    if build.include? "cocoa"
+    if build.with? "cocoa"
       args << "--with-ns" << "--disable-ns-self-contained"
       system "./configure", *args
       system "make"
@@ -90,7 +88,7 @@ class Emacs < Formula
         exec #{prefix}/Emacs.app/Contents/MacOS/Emacs -nw  "$@"
       EOS
     else
-      if build.with? "x"
+      if build.with? "x11"
         # These libs are not specified in xft's .pc. See:
         # https://trac.macports.org/browser/trunk/dports/editors/emacs/Portfile#L74
         # https://github.com/Homebrew/homebrew/issues/8156
@@ -108,14 +106,14 @@ class Emacs < Formula
 
     # Follow MacPorts and don't install ctags from Emacs. This allows Vim
     # and Emacs and ctags to play together without violence.
-    unless build.include? "keep-ctags"
+    if build.without? "ctags"
       (bin/"ctags").unlink
       (man1/"ctags.1.gz").unlink
     end
   end
 
   def caveats
-    if build.include? "cocoa" then <<-EOS.undent
+    if build.with? "cocoa" then <<-EOS.undent
       A command line wrapper for the cocoa app was installed to:
         #{bin}/emacs
       EOS
